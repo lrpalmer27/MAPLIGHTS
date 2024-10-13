afb58b4d63dee596af0bb12e8e74c2ce50a83e5c
lrpalmer27 <lrpalmer@mun.ca>
2024-10-12 20:02:35 -0500

    gets thru about 28 cities, then reaches an error where it cant compare station latest hourly date with current date because of a no date entry

--- GetandPlotCities_fromListofCoords.py
+++ GetandPlotCities_fromListofCoords.py
@@ -1,12 +1,15 @@
 # Import Meteostat library and dependencies
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import pandas as pd
 import matplotlib.pyplot as plt
 from meteostat import Stations, Hourly
 import os
 
+#preamble
 debugging=1
 Hourly.cache_dir=r'.'
+WS=pd.DataFrame()
+stations = Stations()
 
 ## ---------------------- GET LATLONG FROM CSV FILE --------------------------------
 df = pd.read_csv(os.path.join(os.getcwd(),'NA_Cities.csv'))
@@ -18,24 +21,46 @@ if debugging:
     print(df.LONG)
 
     print(df.loc[1,'LONG'])
+    
+## --------------------- OPEN LOOP TO GRAB RELEVANT DATA FROM EACH OF THESE LOCATIONS ------------------------------
+# init empty lists to append data to
+ICAO=[]
+COORDS=[]
+CTEMP=[]
+SNOW=[]
+for i in range(0,Nrows):
+        
+    ## ---------------------- GET WEATHER STATION CLOSEST TO THESE POINTS (FROM CSV) -----------------
+    stations = stations.nearby(df.loc[i,'LAT'],-df.loc[i,'LONG'])
+    station = stations.fetch(1)
+    
+    if debugging:
+        print(station)
+        
+    loop=2
+    while station['hourly_end'].iloc[-1].date() < (datetime.now().date()-timedelta(days=60)):
+            # print(station['hourly_end'].iloc[-1].date())
+            station=stations.fetch(loop)
+            # print(station['hourly_end'].iloc[-1].date())
+            loop+=1 
 
-## ---------------------- GET WEATHER STATION CLOSEST TO THESE POINTS (FROM CSV) -----------------
+    ## ---------------------- GET WEATHER STATION DATA IN DICT & ADD TO DF -----------------
+    data = Hourly(station,datetime.now()-timedelta(hours=1),datetime.now())
+    data=data.fetch()
+    
+    if debugging:
+        print(data)
 
-stations = Stations()
-stations = stations.nearby(df.loc[0,'LAT'],-df.loc[0,'LONG'])
-station = stations.fetch(1)
+    ICAO.append(station.icao[0])
+    COORDS.append([station.latitude[0],station.longitude[0]])
+    CTEMP.append(data['temp'].iloc[0])
+    SNOW.append(data['snow'].iloc[0])
+    
+    if debugging:
+        print('current lists')
+        print(ICAO,COORDS,CTEMP,SNOW)
+    
 
-if debugging:
-    print(station)
 
-## ---------------------- GET WEATHER STATION DATA IN DICT & ADD TO DF -----------------
-
-data = Hourly(station,datetime.now()-timedelta(hours=1),datetime.now())
-data=data.fetch()
-
-if debugging:
-    print(data)
-
-WS=pd.DataFrame()
-
+# add2DF={'ICAO':station.icao[0],'Coords':[station.latitude[0],station.longitude[0]],'Ctemp':data['temp'].iloc[0],'Snow':data['snow'].iloc[0]}
 print('end')
--- WeatherStationValidation.py
+++ WeatherStationValidation.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 from meteostat import Stations, Hourly
 
 stations = Stations()
-stations = stations.nearby(48.94, -54.57) #dead center canada to use as starting point
+stations = stations.nearby(32.4333, -104.25) #dead center canada to use as starting point
 station = stations.fetch(1)
 
 print(station)
 
-Hourly.cache_dir=r'C:\Users\logan\Desktop\Projects\WallMap_lights'
+Hourly.cache_dir=r'.'
 
 data = Hourly(station,datetime.now()-timedelta(hours=1),datetime.now())
 data=data.fetch()
