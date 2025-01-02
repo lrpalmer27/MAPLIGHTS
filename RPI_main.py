53fcef89e4a8e0ba7140ed5a52b106267dc6e927
lrpalmer27 <lrpalmer@mun.ca>
2025-01-01 18:01:25 -0600

    some funky logic going on. cleaned up

--- RPI_main.py
+++ RPI_main.py
@@ -25,7 +25,7 @@ LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating a signal (try 10)
-LED_BRIGHTNESS = 100      # Set to 0 for darkest and 255 for brightest - THIS IS MAPPED LATER. USE THIS AS A GLOBAL BRIGHTNESS MODIFIER.
+LED_BRIGHTNESS = 80      # Set to 0 for darkest and 255 for brightest - THIS IS MAPPED LATER. USE THIS AS A GLOBAL BRIGHTNESS MODIFIER.
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -33,14 +33,14 @@ def QuickLoop(Data,strip,startingTime,cUTCtime,BuildLocationSunrise,BuildLocatio
     """
     LOOPDURATION: time in minutes before this function is killed and new data is re-generated.
     """
-    while cUTCtime + timedelta(minutes=LOOPDURATION) > startingTime: #this opens a while loop for 1 hour.
+    while startingTime + timedelta(minutes=LOOPDURATION) < cUTCtime: #this opens a while loop for 1 hour.
         # # Check current time
         cUTCtime=datetime.now(timezone.utc)
         
         if BuildLocationSunrise<=cUTCtime>=BuildLocationSunset:
             localDaylight=1
         else:
-            localDaylight=0.5 #Use this to modify how much less bright it gets.
+            localDaylight=0.3 #Use this to modify how much less bright the whole board gets.
             
         # # Go though each row in pkl df and determine colour considerate of local and station sunset times. 
         for rowN in range(0,len(Data.Sunrise)):
