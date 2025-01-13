4853a85438072eec7797572abf70ebb6800c00cc
lrpalmer27 <lrpalmer@mun.ca>
2025-01-12 20:11:34 -0600

    adding logging to troubleshoot why its not booting. seems to run, but screen isnt intializing

--- BootMap.sh
+++ BootMap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo 'Start'
+echo 'Starting map'
 sleep 10
-screen -S main -d -m bash -c "sudo python3 ~/Desktop/LIGHTS/RPI_main.py"
-echo 'Started map'
+screen -S main -d -m bash -c "sudo python3 ~/Desktop/LIGHTS/RPI_main.py" >> /var/log/bootmapMain.log 2>&1 &
+echo 'Map started sucessfully - now exiting'
+
+exit 0
